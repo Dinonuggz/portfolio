db6c71047242f9989926f83f64e55d6f04d2d788
Adrian Petersson <adriantimothypetersson@gmail.com>
2021-03-27 10:56:26 +0100

    new links

--- src/data/index.js
+++ src/data/index.js
@@ -34,7 +34,7 @@ export default {
         title: 'projects',
         projectData:[
             {id:"f247", category: "Research UX UI", name:"Fitness 24 Seven Redesign", desc: "A redesign of the current app of a popular Scandinavian fitness chain. The objective was to enhance the overall user experience by making the app more useful to all members.", link:"f247",img:f247hero, external:"Live Prototype", ext:"https://www.figma.com/proto/0hdiXgXRgr9iMXYLZ6BCVl/Fitness24%2F7-Redesign?kind=&node-id=626%3A605&scaling=scale-down&viewport=400%2C914%2C0.26390525698661804"},
-            {id:"astra", category: "UI BRANDING FRONT-END", name:"Astra Watches E-Commerce", desc: "Astra Watches is a personal project based on my own watch designs. This project explores e-commerce landing-page designs suitable for the brand image.", link:"astra", img:astrahero, external:"Live Website", ext:"https://dinonuggz.github.io/astra/" },
+            {id:"astra", category: "UI BRANDING FRONT-END", name:"Astra Watches E-Commerce", desc: "Astra Watches is a personal project based on my own watch designs. This project explores e-commerce landing-page designs suitable for the brand image.", link:"astra", img:astrahero, external:"Live Website", ext:"https://astrawatches.netlify.app/" },
             {id:"portfolio", category: "UI FRONT-END", name:"Portfolio", desc: "This responsive portfolio you're currently viewing was designed, wireframed and ultimately developed using React & Tailwind CSS.", link:"portfolio", img:portfoliohero, external:"Currently viewing!" },
         ],
       }
